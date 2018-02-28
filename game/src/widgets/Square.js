8b49ced3c33cfb7e5292d0071dfa0990828c2974
Yulia <yulia.scherbina@gmail.com>
2018-02-28 14:38:38 +0100

    Square as an interactive component

--- game/src/widgets/Square.js
+++ game/src/widgets/Square.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
 export default class Square extends Component {
+  state = {
+    value: null
+  }
+
   render() {
     const { value } = this.props;
 
     return (
-      <button className='square'>
-          { value }
+      <button 
+          className='square'
+          onClick={() => this.setState({value: 'X'})}>
+          { this.state.value }
       </button>
     )
   }
